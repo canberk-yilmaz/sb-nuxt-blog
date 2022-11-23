fea6b27b7d9679e0fee871ff6a757f8a1b783860
canberk_yilmaz <canberk.yilmaz@reengen.com>
2022-11-23 16:33:26 +0300

    storyblok connection and tailwind add

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+  modules: [
+    ["@storyblok/nuxt", { accessToken: `${process.env.SB_KEY}` }],
+    "@nuxtjs/tailwindcss",
+  ],
+});
